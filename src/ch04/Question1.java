3264a131dd5f4034009f51d37c0f49bad560413e
DESKTOP-1FBOVK1\rosybarb51 <liloding@naver.com>
2021-01-15 01:00:05 +0900

    자바 ppt ch04 수업 파일

---
+++ src/ch04/Question1.java
@@ -0,0 +1,16 @@
+package ch04;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 문제 1) 램덤 숫자를 0 ~ 9까의 숫자 1개를 생성하고 사용자 입력을 하나 받아서 컴퓨터가 저장한 숫자와 같을 경우 '맞췄다' 다를 경우 '꽝 다음기회를' 이라는 문장을 출력하는 프로그램을 작성하세요
+		// 랜덤 숫자 : Math.random() : 0.0 ~ 0.9까지 출력
+		//	(int)(Math.random() * 최대값) + 최소값
+		
+		// 사용자 입력 : import java.util.Scanner;
+		// Scanner sc = new Scanner(System.in);
+		// sc.next(); 공백문자가 나올때까지의 문자를 입력받음
+		// sc.nextInt() : 공백문자가 나올때까지의 숫자를 입력받음
+	}
+}
